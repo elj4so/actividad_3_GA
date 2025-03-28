e04b4f38951eef2d355f2484dd18588507fe7b46
Jasiel Solís García <Jasiel>
2025-03-27 18:54:24 -0600

    second commit

--- src/analytics.js
+++ src/analytics.js
@@ -1,21 +1,20 @@
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
-// Initialize ReactGA con tu ID de seguimiento de Google Analytics
 const initializeAnalytics = () => {
-    ReactGA.initialize('G-N9HVPV7JXV'); // Reemplaza con tu ID de Google Analytics
+  ReactGA.initialize('G-N9HVPV7JXV'); // Tu ID de GA4
 };
 
-// Función para rastrear vistas de páginas
 const trackPageView = (page) => {
-    ReactGA.pageview(page);
+  ReactGA.send({ hitType: "pageview", page });
 };
 
-// Función para rastrear eventos personalizados
-const trackEvent = (category, action) => {
-    ReactGA.event({
-        category: category,
-        action: action,
-    });
+const trackEvent = (category, action, label, value) => {
+  ReactGA.event({
+    category,
+    action,
+    label,
+    value
+  });
 };
 
-export { initializeAnalytics, trackPageView, trackEvent };
+export { initializeAnalytics, trackPageView, trackEvent }
